69f84a9811ab7fc6e259579cad91975f3b7242fd
Maciej Małecki <me@mmalecki.com>
2016-08-11 00:54:46 +0200

    Add a `normal` feed type

--- index.js
+++ index.js
@@ -2,15 +2,26 @@
 
 const util = require('util')
 const assert = require('assert')
-const Transform = require('stream').Transform
+const Duplex = require('stream').Duplex
 
 const ChangesResponse = module.exports = function(options) {
   const self = this
 
-  self._type = options.type || 'normal'
-  self._heartbeat = options.heartbeat || 60000
+  Duplex.call(self, { objectMode: true })
 
-  assert(self._type === 'continuous', 'only continuous feed type is supported')
+  self._type = options.type || 'normal'
+  assert(
+    ['continuous', 'normal'].indexOf(self._type) !== -1,
+    'only normal and continuous feed type are supported'
+  )
+
+  // CouchDB doesn't throw when passed a heartbeat with normal feed type, so
+  // we don't either.
+  self._heartbeat = options.heartbeat || 60000
+  self._lastSeq = options.since || 0
+  self._isFirstSeq = true
+
+  if (self._type === 'normal') self.push('{"results":[\n');
 
   if (self._heartbeat) {
     self._heartbeatInterval = setInterval(self._writeHeartbeat.bind(self), self._heartbeat)
@@ -19,16 +30,38 @@ const ChangesResponse = module.exports = function(options) {
     })
   }
 
-  Transform.call(self, { objectMode: true })
+  if (this._type === 'normal') this.end = ChangesResponse.prototype._normalEnd
 }
-util.inherits(ChangesResponse, Transform)
+util.inherits(ChangesResponse, Duplex)
 
-ChangesResponse.prototype._transform = function(chunk, encoding, cb) {
+ChangesResponse.prototype._write = function(chunk, encoding, cb) {
+  var stringified = JSON.stringify(chunk)
+
+  this._lastSeq = chunk.seq
+
   if (this._type === 'continuous')
-    this.push(JSON.stringify(chunk) + '\n')
+    this.push(stringified + '\n')
+  else if (this._type === 'normal') {
+    if (!this._isFirstSeq) this.push(',\n')
+    this.push(stringified)
+  }
+
+  this._isFirstSeq = false
+
   cb()
 }
 
+ChangesResponse.prototype._read = function () {}
+
 ChangesResponse.prototype._writeHeartbeat = function() {
   this.push('\n')
+}
+
+ChangesResponse.prototype._normalEnd = function (chunk, encoding, cb) {
+  var self = this
+
+  self._write(chunk, encoding, () => {
+    self.push('\n],\n"last_seq":' + self._lastSeq + '}')
+    Duplex.prototype.end.call(self, cb)
+  })
 }
